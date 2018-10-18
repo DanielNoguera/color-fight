69cb1e6fe47272031d66d318126504ea1a260854
DanielNoguera <40502780+DanielNoguera@users.noreply.github.com>
2018-10-17 19:03:13 -0700

    Removed useless comments

--- server/server.js
+++ server/server.js
@@ -34,17 +34,4 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
   console.log(`Yo! Daniel!  Server is up on port ${port} !`);
 });
-//to do a complete post, make a .gitignore file
-//put in it node_modules/
-//build a port variable equal to process.env.PORT || make the server.listen to port first
-//go to package.json in scripts make a start equal to node and then down from the folder like server/server.js or such
-//in package.json make engines and set it equal to your node version.  Find this with node --version
-//make a new repo
-//git status
-//git add .
-//git commit -m 'Init commit'
-//git remote add origin https://github.com/DanielNoguera/color-fight.git
-//git push -u origin master
-//heroku create
-//git push heroku master
-//heroku git:remote -a aqueous-cliffs-48607
+
